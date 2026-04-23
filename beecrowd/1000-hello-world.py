168b8771b5fcd252db57692423a47a3bdf21112d
Daniel27122011 <20261ctb0100040@estudantes.ifpr.edu.br>
2026-04-23 20:02:18 +0000

    Aula 10 - 1001: extremante basico

---
+++ beecrowd/1000-hello-world.py
@@ -0,0 +1,15 @@
+'''
+problema: beecrowd | 1000
+Data: 2026.04.09
+Estudante Daniel Gonçalves de Souza
+'''
+# Objetivo: Exibir exatamente a mensagem "Hello World!" na tela 
+
+#---ANÁLISE (LIAC)---
+# Entrada: nenhuma - este problema não requer leitura de dados
+# Processamento: nenhum - não há calculo ou transformação
+# Saida: exibir o texto "Hello World!" exatamente como especificado
+
+# print() é o comando de saida em Python
+# Equivalente ao bloco "diga ..." do Scrath
+print('Hello World!')
